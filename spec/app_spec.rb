37996f31393e58a1fa555207a34958389d6e6ed0
Claudio Neves <claudio.neves@bbc.co.uk>
2012-10-04 14:48:20 +0100

    Initialising stub request before actual request.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,9 +1,13 @@
 describe 'Application' do
   include Rack::Test::Methods
-
+  
+  before (:each) do
+  	@stub_request = my_stub_request
+  end
+  
   it "should provide RDF endpoint" do
     get '/rdf?identifier=http://someresource'
-    stub_response_status.should == "200"
+    @stub_request.response.status.should == "200"
   end
   
 end
