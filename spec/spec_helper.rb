96148375c69dfe2767f2ccc7cff0e9230c84c9f9
Robert Kenny <kenoir@gmail.com>
2012-10-04 16:46:06 +0100

    reordered includes for Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 require 'simplecov'
 
+require_relative '../models/rdf_loader.rb'
+require_relative '../webmock/helpers.rb'
+
+include WebmockHelpers
+include RDF
+
 require './app'
 require 'rack/test'
 require 'webmock/rspec'
 require 'rdf'
 require 'rdf/rdfxml'
 require 'pp'
-
-require_relative '../models/rdf_loader.rb'
-require_relative '../webmock/helpers.rb'
-
-include WebmockHelpers
-include RDF
 
 def app
   Application 
