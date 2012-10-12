1474e36a7a4da9a59f96a45ddda767f34c5a9c39
Robert Kenny <kenoir@gmail.com>
2012-10-12 16:51:52 +0100

    added full description

--- models/blender.rb
+++ models/blender.rb
@@ -39,40 +39,40 @@ class Blender
   private 
   def insert_json_into_graph(rdf_graph,json)
     parsed_json = JSON.parse(json)
-    subject_uri = RDF::URI.new(parsed_json["article"]["url"])
 
-    title_uri = RDF::URI.new("http://purl.org/dc/terms/title")
-    description_uri = RDF::URI.new("http://purl.org/dc/terms/abstract") 
+    subject_uri = RDF::URI.new(parsed_json["article"]["url"])
+    abstract_uri = RDF::URI.new("http://purl.org/dc/terms/abstract") 
+    description_uri = RDF::URI.new("http://purl.org/dc/terms/description")
     image_uri = RDF::URI.new("http://purl.org/dc/dcmitype/image")
 
-    title = description = image = ""
+    abstract = image = description = ""
 
     begin
-      title = parsed_json["article"]["title"] 
-      description = parsed_json["article"]["description"]
+      abstract = parsed_json["article"]["description"]
+      description = parsed_json["article"]["full_data"]["body"]
       image = parsed_json["article"]["image"]["origin"] 
     rescue Exception => e
-      log("Could not parse json: #{parsed_json}",e)
+      log("Error parsing JSON: #{parsed_json}",e)
     end
 
     # Use placeholder image if empty!
     image = "http://news-labs-events-prototype.herokuapp.com/images/event/event-placeholder.jpeg" if image.empty?
 
     triples = [
-      {
+     {
         :subject => subject_uri,        
-        :predicate => title_uri,        
-        :object => RDF::Literal.new(title)
-      },
-      {
-        :subject => subject_uri,        
-        :predicate => description_uri,  
-        :object => RDF::Literal.new(description)
+        :predicate => abstract_uri,  
+        :object => RDF::Literal.new(abstract)
       },
       {
         :subject => subject_uri,     
         :predicate => image_uri,        
         :object => RDF::URI.new(image)  
+      },
+      {
+        :subject => subject_uri,     
+        :predicate => description_uri,
+        :object => RDF::Literal.new(description)  
       }
     ] 
 
